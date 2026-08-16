03971267692f1553b56f102335f8b4e5108020bd
Celebrityceleb <ajayijames258@gmail.com>
2026-08-16 21:58:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,2 @@
-const talkButton = document.getElementById("talkButton");
-const response = document.getElementById("response");
-const status = document.getElementById("status");
-
-function jarvisSpeak(text) {
-    const voice = new SpeechSynthesisUtterance(text);
-
-    voice.lang = "en-US";
-    voice.rate = 0.95;
-    voice.pitch = 0.9;
-    voice.volume = 1;
-
-    window.speechSynthesis.cancel();
-    window.speechSynthesis.speak(voice);
-}
-
-talkButton.addEventListener("click", () => {
-
-    status.textContent = "JARVIS ACTIVE";
-
-    const message =
-        "Hello. I am Jarvis. Your system is online.";
-
-    response.textContent = message;
-
-    jarvisSpeak(message);
-});
+const SUPABASE_URL = "https://oechiufoqtnuofoiatgw.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Pf0mP9qTvCdWCUjZTt-xrA_G4gch1w8";
