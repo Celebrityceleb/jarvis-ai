e5c294c1069f7b90346e7fa196cf4f3dbaf84e78
Celebrityceleb <ajayijames258@gmail.com>
2026-08-17 09:58:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const SUPABASE_URL = "https://oechiufoqtnuofoiatgw.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_Pf0mP9qTvCdWCUjZTt-xrA_G4gch1w8";
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
